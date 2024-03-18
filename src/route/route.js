a61bbb32f8ebfffc0771d9a7f9b5e41e0dbfcdbb
adityasingh-fun <130737541+adityasingh-fun@users.noreply.github.com>
2024-03-18 16:28:14 +0530

    Update route.js

--- src/route/route.js
+++ src/route/route.js
@@ -3,6 +3,9 @@ const requestController = require('../controllers/requestController.js');
 
 const router = express.Router();
 
-router.post('/getCity',requestController.gettingCities);
+// router to get data based on city name
+router.post('/getCity', requestController.gettingCities);
 
+// API to get nearest five locations based on latitude and longitude passed
+router.post('/getNearestLocations', requestController.getNearestLocations);
 module.exports = router;
