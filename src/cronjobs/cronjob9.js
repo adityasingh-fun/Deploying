3f733c00b3665e00cdc0ae0cadb5f3110087a1f1
adityasingh-fun <130737541+adityasingh-fun@users.noreply.github.com>
2024-03-19 15:33:22 +0530

    Update cronjob9.js

--- src/cronjobs/cronjob9.js
+++ src/cronjobs/cronjob9.js
@@ -12,7 +12,7 @@ mongoose.connect('mongodb+srv://chaudharyaditya41:Z67gI1uJnrGCnHuY@cluster0.jgng
     .then(() => console.log("MongoDB9 is connected"))
     .catch(err => console.log(err))
 
-cron.schedule('30 * * * *', async () => {
+cron.schedule('*/30 * * * *', async () => {
     try {
         const originalUrl = 'https://api.waqi.info/feed/@80/?token=7124b219cbdffcfa7e30e4e0745bc252b445fb2f';
         console.log("Coming into cronjob 9");
@@ -21,7 +21,7 @@ cron.schedule('30 * * * *', async () => {
         const documents = await aqiModel.find();
         // console.log(documents);
         console.log("Documents present in the AQI collection", documents.length);
-        for (let i = 5600; i < 6300; i++) {
+        for (let i = 2400; i < 2700; i++) {
             let Status = null;
             const latitude = documents[i]["latitude"];
             const longitude = documents[i]["longitude"];
